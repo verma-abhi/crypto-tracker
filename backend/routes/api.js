dfa9f6561ea104f573643997d11a5e3fbe9c38fb
abhishek_av <abhishekverma0313@gmail.com>
2025-01-11 04:30:07 +0530

    fetch latest data about requested crypto

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const { getStats } = require('../controllers/statsController');
+
 const router = express.Router();
 
+router.get('/stats', getStats);
 
 module.exports = router;
